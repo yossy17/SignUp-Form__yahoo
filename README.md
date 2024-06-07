f3aab8646d8da3d46e8cc5b829c21a1d782f8d40
yocci_ii <hiitoy0117@gmail.com>
2024-06-07 17:31:08 +0900

    commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # SignUpForm
+
+https://codepen.io/dfupttsl-the-scripter/pen/MWdoLLW
--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
-const form = document.querySelector('.validationForm');
-const useridInput = document.getElementById('userid');
-const emailInput = document.getElementById('email');
-const passwordInput = document.getElementById('password');
-const useridError = document.getElementById('useridError');
-const emailError = document.getElementById('emailError');
-const passwordError = document.getElementById('passwordError');
+const form = document.querySelector(".validationForm");
+const useridInput = document.getElementById("userid");
+const emailInput = document.getElementById("email");
+const passwordInput = document.getElementById("password");
+const useridError = document.getElementById("useridError");
+const emailError = document.getElementById("emailError");
+const passwordError = document.getElementById("passwordError");
 
-form.addEventListener('submit', (event) => {
+form.addEventListener("submit", (event) => {
   event.preventDefault();
   validateForm();
 });
@@ -17,62 +17,65 @@ function validateForm() {
   // UserID 検証
   const userid = useridInput.value.trim();
   if (!userid) {
-    showError(useridInput, useridError, 'User IDは必須項目です。');
+    showError(useridError, "User IDは必須項目です。");
     isValid = false;
   } else if (
     /[\u3000-\u303F\u3040-\u309F\u30A0-\u30FF\uFF00-\uFFEF\u4E00-\u9FAF\u2605-\u2606\u2190-\u2195\u203B]/g.test(
       userid
     )
   ) {
-    showError(useridInput, useridError, 'User IDは半角英数字のみで入力してください。');
+    showError(useridError, "User IDは半角英数字のみで入力してください。");
     isValid = false;
   } else if (userid.length < 3 || userid.length > 10) {
-    showError(useridInput, useridError, 'User IDは3文字以上10文字以内で入力してください。');
+    showError(useridError, "User IDは3文字以上10文字以内で入力してください。");
     isValid = false;
   } else if (/[!@#$%^&*()_+\-=\[\]{};:"\\|,.<>\/?]+/.test(userid)) {
-    showError(useridInput, useridError, 'User IDは特殊記号を使用しないでください。');
+    showError(useridError, "User IDは特殊記号を使用しないでください。");
     isValid = false;
   } else {
-    clearError(useridInput, useridError);
+    clearError(useridError);
   }
 
   // MailAddress 検証
   const email = emailInput.value.trim();
   if (!email) {
-    showError(emailInput, emailError, 'MailAddressは必須項目です。');
+    showError(emailError, "MailAddressは必須項目です。");
     isValid = false;
   } else if (
     /[\u3000-\u303F\u3040-\u309F\u30A0-\u30FF\uFF00-\uFFEF\u4E00-\u9FAF\u2605-\u2606\u2190-\u2195\u203B]/g.test(
       email
     )
   ) {
-    showError(emailInput, emailError, 'MailAddressは半角英数字で入力してください。');
+    showError(emailError, "MailAddressは半角英数字で入力してください。");
     isValid = false;
-  } else if (!/^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,3}$/i.test(email)) {
-    showError(emailInput, emailError, '正しいMailAddressを入力してください。');
+  } else if (!/^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/i.test(email)) {
+    showError(emailError, "正しいMailAddressを入力してください。");
     isValid = false;
   } else {
-    clearError(emailInput, emailError);
+    clearError(emailError);
   }
 
   // PassWord 検証
   const password = passwordInput.value.trim();
   if (!password) {
-    showError(passwordInput, passwordError, 'PassWordは必須項目です。');
+    showError(passwordError, "PassWordは必須項目です。");
     isValid = false;
   } else if (password.length < 3 || password.length > 10) {
-    showError(passwordInput, passwordError, 'PassWordは3文字以上10文字以内で入力してください。');
-
-    isValid = false;
-  } else if (!/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{3,10}$/.test(password)) {
     showError(
-      passwordInput,
       passwordError,
-      'PassWordは大文字小文字数字の組み合わせで入力してください。'
+      "PassWordは3文字以上10文字以内で入力してください。"
+    );
+    isValid = false;
+  } else if (
+    !/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{3,10}$/.test(password)
+  ) {
+    showError(
+      passwordError,
+      "PassWordは大文字小文字数字の組み合わせで入力してください。"
     );
     isValid = false;
   } else {
-    clearError(passwordInput, passwordError);
+    clearError(passwordError);
   }
 
   if (isValid) {
@@ -82,10 +85,10 @@ function validateForm() {
 
 function showError(errorElement, message) {
   errorElement.textContent = message;
-  errorElement.style.display = 'block';
+  errorElement.style.display = "block";
 }
 
 function clearError(errorElement) {
-  errorElement.textContent = '';
-  errorElement.style.display = 'none';
+  errorElement.textContent = "";
+  errorElement.style.display = "none";
 }
