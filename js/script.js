2d7d20d4c7cf8d754169a54475d3a420cbe208be
yocci_ii <hiitoy0117@gmail.com>
2024-06-09 22:53:34 +0900

    commit

--- js/script.js
+++ js/script.js
@@ -18,53 +18,53 @@ function validateForm() {
   // UserID 検証
   const userid = useridInput.value.trim();
   if (!userid) {
-    showError(useridError, "User IDは必須項目です。");
+    showError(useridError, "UserIDは必須項目です。");
     isValid = false;
   } else if (
     /[\u3000-\u303F\u3040-\u309F\u30A0-\u30FF\uFF00-\uFFEF\u4E00-\u9FAF\u2605-\u2606\u2190-\u2195\u203B]/g.test(
       userid
     )
   ) {
-    showError(useridError, "User IDは半角英数字のみで入力してください。");
+    showError(useridError, "UserIDは半角英数字のみで入力してください。");
     isValid = false;
   } else if (userid.length < 3 || userid.length > 10) {
-    showError(useridError, "User IDは3文字以上10文字以内で入力してください。");
+    showError(useridError, "UserIDは3文字以上10文字以内で入力してください。");
     isValid = false;
-  } else if (/[!@#$%^&*()_+\-=\[\]{};:"\\|,.<>\/?]+/.test(userid)) {
-    showError(useridError, "User IDは特殊記号を使用しないでください。");
+  } else if (/[!@#$%^&\*()-=\_+\[\]{};:"'\\|,.<>\/?`~]+/.test(userid)) {
+    showError(useridError, "UserIDは特殊記号を使用しないでください。");
     isValid = false;
   } else {
     clearError(useridError);
   }
 
-  // MailAddress 検証
+  // Email 検証
   const email = emailInput.value.trim();
   if (!email) {
-    showError(emailError, "MailAddressは必須項目です。");
+    showError(emailError, "Emailは必須項目です。");
     isValid = false;
   } else if (
     /[\u3000-\u303F\u3040-\u309F\u30A0-\u30FF\uFF00-\uFFEF\u4E00-\u9FAF\u2605-\u2606\u2190-\u2195\u203B]/g.test(
       email
     )
   ) {
-    showError(emailError, "MailAddressは半角英数字で入力してください。");
+    showError(emailError, "Emailは半角英数字で入力してください。");
     isValid = false;
   } else if (!/^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/i.test(email)) {
-    showError(emailError, "正しいMailAddressを入力してください。");
+    showError(emailError, "正しいEmailを入力してください。");
     isValid = false;
   } else {
     clearError(emailError);
   }
 
-  // PassWord 検証
+  // Password 検証
   const password = passwordInput.value.trim();
   if (!password) {
-    showError(passwordError, "PassWordは必須項目です。");
+    showError(passwordError, "Passwordは必須項目です。");
     isValid = false;
   } else if (password.length < 3 || password.length > 10) {
     showError(
       passwordError,
-      "PassWordは3文字以上10文字以内で入力してください。"
+      "Passwordは3文字以上10文字以内で入力してください。"
     );
     isValid = false;
   } else if (
@@ -72,7 +72,7 @@ function validateForm() {
   ) {
     showError(
       passwordError,
-      "PassWordは大文字小文字数字の組み合わせで入力してください。"
+      "Passwordは大文字小文字数字の組み合わせで入力してください。"
     );
     isValid = false;
   } else {
